79d72ba87f4afba76a6c61f53a3eaaf4b2ee0614
Kristen <kristenarguello20@gmail.com>
2024-08-06 00:26:52 -0300

    Add comments to indicate what is being done in the code

--- app/memory_alloc.py
+++ app/memory_alloc.py
@@ -8,34 +8,45 @@ class SimulateMemoryAllocator:
         self.num_pages = 2 ** (self.virtual_mem_size - self.page_size)
         self.num_frames = 2 ** (self.physical_mem_size - self.page_size)
 
+        # creates a list of -1s with the size of the number of pages to represent the page table (virtual memory)
         self.page_table = [-1] * self.num_pages
-        self.memory = [0] * physical_mem
+        # creates a list of 0s with the size of the number of frames to represent the physical memory
+        self.memory = [0] * self.num_frames
 
     def get_physical_address(self, virtual_address):
-        # checks if the virtual address is within the range
+        # checks if the virtual address is within the range of the virtual memory size
         if virtual_address >= 2**self.virtual_mem_size:
             raise ValueError("Virtual address is out of range")
 
-        # for each >> = division by 2
-        # for each & = modulo by 2
-        # for each << = multiplication by 2
-        # for each | = addition
-        page_number = virtual_address >> self.page_size
-        offset = virtual_address & ((1 << self.page_size) - 1)
+        # >> = division
+        # & = modulo
+        # << = multiplication
+        # | = addition
+        page_number = virtual_address >> self.page_size  # gets the page number
+        mask = (1 << self.page_size) - 1
+        offset = virtual_address & mask  # gets the offset
 
-        if self.page_table[page_number] == -1:
+        # gets the value in the page table position to the page number
+        frame_number = self.page_table[page_number]
+
+        # if its -1, it means that the page is not in the physical memory yet
+        if frame_number == -1:
             try:
+                # looks for a free frame in the physical memory
                 free_frame = self.memory.index(0)
             except ValueError:
                 raise MemoryError("No more free frames in the physical memory")
 
+            # updates the page table with the free frame
             self.page_table[page_number] = free_frame
+            # updates the physical memory to indicate it is being used
             self.memory[free_frame] = 1
 
-        frame_number = self.page_table[page_number]
-        return (frame_number << self.page_size) | offset  # physical address
+        # physical address to be returned
+        return (frame_number << self.page_size) | offset
 
     def simulate_list_addresses(self, virtual_addresses):
+        # gets the physical address for each virtual address that is passed in the list
         return [
             self.get_physical_address(virtual_address)
             for virtual_address in virtual_addresses
