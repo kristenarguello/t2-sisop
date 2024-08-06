e3ac5558de494bda307907a8cb3d02ae42384b14
Kristen <kristenarguello20@gmail.com>
2024-08-05 22:29:57 -0300

    Add memory allocator simulator class

--- app/memory_alloc.py
+++ app/memory_alloc.py
@@ -1,20 +1,42 @@
-# copilot did it
 class SimulateMemoryAllocator:
-    def __init__(self, memory_size):
-        self.memory_size = memory_size
-        self.memory = [0] * memory_size
+    def __init__(self, virtual_mem, physical_mem, page_size):
+        self.virtual_mem_size = virtual_mem
+        self.physical_mem_size = physical_mem
+        self.page_size = page_size
 
-    def allocate(self, size):
-        for i in range(self.memory_size - size + 1):
-            if all(self.memory[i + j] == 0 for j in range(size)):
-                for j in range(size):
-                    self.memory[i + j] = 1
-                return i
-        return -1
+        # uses bit manipulation
+        self.num_pages = 2 ** (self.virtual_mem_size - self.page_size)
+        self.num_frames = 2 ** (self.physical_mem_size - self.page_size)
 
-    def deallocate(self, start, size):
-        for i in range(size):
-            self.memory[start + i] = 0
+        self.page_table = [-1] * self.num_pages
+        self.memory = [0] * physical_mem
 
-    def print_memory(self):
-        print(self.memory)
+    def get_physical_address(self, virtual_address):
+        # checks if the virtual address is within the range
+        if virtual_address >= 2**self.virtual_mem_size:
+            raise ValueError("Virtual address is out of range")
+
+        # for each >> = division by 2
+        # for each & = modulo by 2
+        # for each << = multiplication by 2
+        # for each | = addition
+        page_number = virtual_address >> self.page_size
+        offset = virtual_address & ((1 << self.page_size) - 1)
+
+        if self.page_table[page_number] == -1:
+            try:
+                free_frame = self.memory.index(0)
+            except ValueError:
+                raise MemoryError("No more free frames in the physical memory")
+
+            self.page_table[page_number] = free_frame
+            self.memory[free_frame] = 1
+
+        frame_number = self.page_table[page_number]
+        return (frame_number << self.page_size) | offset  # physical address
+
+    def simulate_list_addresses(self, virtual_addresses):
+        return [
+            self.get_physical_address(virtual_address)
+            for virtual_address in virtual_addresses
+        ]
