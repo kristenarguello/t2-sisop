065baac9e2e2d08e513d8db4922bb6059ec2a869
Kristen <kristenarguello20@gmail.com>
2024-08-05 22:30:14 -0300

    Add the memory allocator usage in main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from .utils import address_transformer_str_to_bit
-
+from utils import address_transformer_str_to_bit
+from memory_alloc import SimulateMemoryAllocator
 
 print("** MEMORY MANAGER SIMULATOR **")
 virtual_mem_size = input("Enter the virtual memory size: 2^")
@@ -18,3 +18,10 @@ str_virtual_addresses = input(
 bits_virtual_addresses = address_transformer_str_to_bit(str_virtual_addresses)
 
 # need to use the memory allocator
+mem_alloc = SimulateMemoryAllocator(virtual_mem_size, physical_mem_size, page_size)
+physical_addresses = mem_alloc.simulate_list_addresses(bits_virtual_addresses)
+
+print("The virtual addresses are:", bits_virtual_addresses)
+print("The physical addresses are:", physical_addresses)
+print("The page table is:", mem_alloc.page_table)
+print("The physical memory is:", mem_alloc.memory)
